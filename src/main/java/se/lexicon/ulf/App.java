48d6f4f4b266c16b6d2c4cc7c52e789e4b6bd747
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-08-23 15:44:20 +0200

    added new massage

--- src/main/java/se/lexicon/ulf/App.java
+++ src/main/java/se/lexicon/ulf/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello GitHub!" );
+
+        System.out.println( "Here is my first update to my repo" );
     }
 }
