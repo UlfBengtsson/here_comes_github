16bcee3fe48f6d96692c825fa9395ec323f64b94
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-08-26 11:47:11 +0200

    added Array of cars and print them

--- src/main/java/se/lexicon/ulf/App.java
+++ src/main/java/se/lexicon/ulf/App.java
@@ -8,8 +8,30 @@ public class App
 {
     public static void main( String[] args )
     {
+        //String[] cars = {"Volvo 740", "Saab 900","Audi S4", "Tesla Model 3"};
+        String[] cars = new String[4];
+
+        cars[0] = "Volvo 740";
+        cars[1] = "Saab 900";
+        cars[2] = "Audi S4";
+        cars[3] = "Tesla Model 3";
+
         System.out.println( "Hello GitHub!" );
 
         System.out.println( "Here is my first update to my repo" );
-    }
-}
+
+        System.out.println(cars[3]);// prints only one car
+
+        System.out.println("---------------- Car list ----------------------");
+
+        /*
+        for (int i=0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }*/
+
+        for (String aCar : cars ) {
+            System.out.println(aCar);
+        }
+
+    }//End of Main
+}//End of App
